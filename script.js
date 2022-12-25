6a87ec82b2e2847f0bc17b84db09c23fe67ed2a7
Glitch (glitch-hello-website) <none>
2022-12-25 04:15:10 +0000

    👵👂 Checkpoint ./script.js:24111152/1694

--- script.js
+++ script.js
@@ -1,22 +1,25 @@
-window.addEventListener("load", ()=> {
+window.addEventListener("load", () => {
   let long;
   let lat;
-  if(navigator.geolocation){
-    navigator.geolocation.getCurrentPosition
-    (position => {
+  if (navigator.geolocation) {
+    navigator.geolocation.getCurrentPosition((position) => {
       //console.log(position);
       long = position.coords.longitude;
       lat = position.coords.latitude;
+
+      const api = `https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/${lat}%2C%20${long}?unitGroup=us&key=4MHU9N92TYVGPA9JNANU6MQYQ&contentType=json`;
+
+      fetch(api)
+        .then((response) => {
+          return response.json();
+        })
+        .then((data) => {
+          console.log(data);
+        
+          const {} = data.currentConditions;
+        
+        });
       
-      const api = `https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/${lat}%2C%20${long}?unitGroup=us&key=4MHU9N92TYVGPA9JNANU6MQYQ&contentType=json`;   
     });
-    
-    fetch(api)
-      .then(response => {
-      return response.json();
-    })
-    
   }
-  
-  
 });
