fa86cc71970d330a0bc96c19ad8d8a449e599cb4
Glitch (glitch-hello-website) <none>
2022-12-25 07:46:10 +0000

    👾🙏 Checkpoint ./script.js:24111152/9291 ./index.html:24111152/50

--- script.js
+++ script.js
@@ -8,7 +8,9 @@ window.addEventListener("load", () => {
   let locationTimezone = document.querySelector(".location-timezone");
   let temperatureSection = document.querySelector(".temperature-section");
   let temperatureSpan = document.querySelector(".temperature-section span");
-
+  
+  let img = document.getElementById("locimg")
+  
   /*
   tempSection.addEventListener('click', () => {
             if (tempSpan.textContent === 'F°') {
@@ -35,13 +37,13 @@ window.addEventListener("load", () => {
           console.log(data);
           const { temp, conditions, icon } = data.currentConditions;
           // Set DOM elements from the API
-        
+
           let celsius = Number(((temp - 32) * 5) / 9).toFixed(1);
-        
+
           temperatureDegree.textContent = celsius;
           temperatureDescription.textContent = conditions;
           locationTimezone.textContent = data.timezone;
-          
+
           //Set to switch between C and F on click
           temperatureSection.addEventListener("click", () => {
             if (temperatureSpan.textContent === "F°") {
@@ -52,16 +54,37 @@ window.addEventListener("load", () => {
               temperatureDegree.textContent = temp;
             }
           });
-        
+
           //Set icon
-          /*setIcons(icon,document.querySelector(".icon"))*/
-        
+          /*img.src = setIcons(icon,document.querySelector(".icon"))*/
         });
     });
   }
 
-  /*
-  function setIcons(icon, iconID){
-   
-  }*/
+  function setIcons(icon, iconID) {
+    const currentIcon = (icon) => {
+      switch (icon) {
+        case "clear-day":
+          return "https://cdn.glitch.global/ddf86376-5356-40d5-aea5-6ee7a7dd04dd/clear-day.svg?v=1671944510051";
+        case "clear-night":
+          return "https://cdn.glitch.global/ddf86376-5356-40d5-aea5-6ee7a7dd04dd/clear-night.svg?v=1671944522623";
+        case "partly-cloudy-day":
+          return "https://cdn.glitch.global/ddf86376-5356-40d5-aea5-6ee7a7dd04dd/partly-cloudy-day.svg?v=1671944525795";
+        case "partly-cloudy-night":
+          return "https://cdn.glitch.global/ddf86376-5356-40d5-aea5-6ee7a7dd04dd/partly-cloudy-night.svg?v=1671944529178";
+        case "cloudy":
+          return "https://cdn.glitch.global/ddf86376-5356-40d5-aea5-6ee7a7dd04dd/cloudy.svg?v=1671944541034";
+        case "rain":
+          return "RAIN";
+        case "snow":
+          return "SNOW";
+        case "wind":
+          return "WIND";
+        case "fog":
+          return "FOG";
+        default:
+          return 1;
+      }
+    };
+  }
 });
